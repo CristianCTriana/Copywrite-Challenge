39686ba9ec9cea56ae921b2eb90f53ca203cc8bf
Camilo T <cristianctr97@gmail.com>
2022-07-27 23:01:27 -0500

    Frontend

---
+++ client/src/components/ResultsContainer.js
@@ -0,0 +1,10 @@
+import ItemList from "./ItemList"
+
+export default function ResultsContainer(){
+    return(
+        <div>
+            Container
+            <ItemList></ItemList>
+        </div>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store/index";
+import Home from './pages/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-  <div>
-    <h1>Funciona</h1>
-  </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/home' element={<Home/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </Provider>
 )
