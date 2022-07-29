3d1c882a1b988d7a57b37393890e88e1f801588d
Camilo T <cristianctr97@gmail.com>
2022-07-28 19:37:59 -0500

    More fixed

--- client/src/components/Instrucciones.js
+++ client/src/components/Instrucciones.js
@@ -3,7 +3,7 @@ import  '../css/instrucciones.css';
 export default function Instrucciones(){
     return(
         <div className="containerText">
-            <p className="text-justify lead">ingresa una palabra en el campo de texto y seguido se pulsa el botón de enviar, solamente se aceptan palabras, no se permite el uso de números ni de caracteres especiales.</p>
+            <p className="text-justify">ingresa una palabra en el campo de texto y seguido se pulsa el botón de enviar, solamente se aceptan palabras, no se permite el uso de números ni de caracteres especiales.</p>
         </div>
     )
 }
